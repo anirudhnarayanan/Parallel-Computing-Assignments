ee3e632f9af8cc8412b8af0ae2614a4704a5a4ad
Anirudh Narayanan <anaraya7@uncc.edu>
2019-02-08 03:02:17 -0500

    finished q 3 , with many bugs

--- assignment-3/dynamic/dynamic_sched.cpp
+++ assignment-3/dynamic/dynamic_sched.cpp
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+using namespace std;
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -12,6 +13,10 @@ float f1(float x, int intensity);
 float f2(float x, int intensity);
 float f3(float x, int intensity);
 float f4(float x, int intensity);
+
+
+pthread_mutex_t mutex_iter, mutex_chunk, task_getter_mutex;
+int next_available_beginning, next_available_end;
 
 #ifdef __cplusplus
 }
@@ -43,7 +48,6 @@ float summation_iteration(int begin,int end)
         float a = current_args.a;
         float b= current_args.b;
         int start = begin;
-        int end = end;
         int intensity = current_args.intensity;
         float pre_product = current_args.pre_product;
 	float mysummed_valuei = 0;
@@ -64,11 +68,12 @@ float summation_iteration(int begin,int end)
         function_eval = f4(a + (i+0.5)*pre_product,intensity);
         break;
   }
-		  mysummed_value += function_eval;
+
+	          pthread_mutex_lock(&mutex_iter);
+		  summed_value += function_eval;
+		  pthread_mutex_unlock(&mutex_iter);
 }	
-	          //mutex lock here
-		  summed_value += mysummed_value;
-		  //mutex unlock
+	return summed_value;
 }
 
 float summation_thread(int begin,int end)
@@ -80,10 +85,9 @@ float summation_thread(int begin,int end)
         float a = current_args.a;
         float b= current_args.b;
         int start = begin;
-        int end = end;
         int intensity = current_args.intensity;
         float pre_product = current_args.pre_product;
-	float mysummed_valuei = 0;
+	float mysummed_value = 0;
   for(i=start;i<end;i++)
   {
     switch(f)
@@ -115,10 +119,9 @@ float summation_chunk(int begin,int end)
         float a = current_args.a;
         float b= current_args.b;
         int start = begin;
-        int end = end;
         int intensity = current_args.intensity;
         float pre_product = current_args.pre_product;
-	float mysummed_valuei = 0;
+	float mysummed_value = 0;
   for(i=start;i<end;i++)
   {
     switch(f)
@@ -136,39 +139,49 @@ float summation_chunk(int begin,int end)
         function_eval = f4(a + (i+0.5)*pre_product,intensity);
         break;
   }
-    		  //lock mutex here
-		  summed_value += function_eval;
-		  //unlock mutex here
+    mysummed_value += function_eval;
 }	
+
+    		  pthread_mutex_lock(&mutex_chunk);
+		  summed_value += mysummed_value;
+    		  pthread_mutex_unlock(&mutex_chunk);
 		  return summed_value;
 }
 
 void* get_next_task(int &begin, int &end)
 {
 	//mutex lock here
+	pthread_mutex_lock(&task_getter_mutex);
 	begin = next_available_beginning;
 	end = next_available_end;
-	next_available_beginning = next_available_beginning + current_args.granularity
-	if(n - (next_available_beginning + current_args.granularity) >= current_args.granularity)
+	if(end == current_args.n)
+		task_done = true;
+	next_available_beginning = next_available_beginning + current_args.granularity;
+	if(current_args.n - (next_available_beginning + current_args.granularity) >= current_args.granularity)
 	{
-		next_available_end = next_available_beginning + current_args.granularity
+		next_available_end = next_available_beginning + current_args.granularity;
 	}
 	else
-		next_available_end = n;
+		next_available_end = current_args.n;
 	
+	pthread_mutex_unlock(&task_getter_mutex);
+	return NULL;
 	//UNLOCK MUTEX
 	 
 }	
-void* worker_threads_iteration()
+void* worker_threads_iteration(void* sum1)
 {
 	while(task_done!=true)
 	{
 		int begin,end;
 		float mysum;
 		get_next_task(begin,end);
+		cout<<begin;
+		cout<<end;
 		summation_iteration(begin,end);	
 	        		
 	}
+	return NULL;
 
 }
 void* worker_threads_thread(void* mysum)
@@ -181,9 +194,10 @@ void* worker_threads_thread(void* mysum)
 		(*((float*)mysum)) += summation_thread(begin,end);	
 	        		
 	}
+	return NULL;
 
 }
-void* worker_threads_chunk()
+void* worker_threads_chunk(void* sum1)
 {
         while(task_done!=true)
 	{
@@ -193,14 +207,15 @@ void* worker_threads_chunk()
 		summation_chunk(begin,end);	
 	        		
 	}
+	return NULL;
 
 }
 int main (int argc, char* argv[]) {
-
   if (argc < 9) {
     std::cerr<<"usage: "<<argv[0]<<" <functionid> <a> <b> <n> <intensity> <nbthreads> <sync> <granularity>"<<std::endl;
     return -1;
   }
+
 
   int functionid = atoi(argv[1]); //Converting function ID from char* to int
   float a = atof(argv[2]); //Converting a value to float as per question
@@ -211,29 +226,71 @@ int main (int argc, char* argv[]) {
   char* sync = argv[7];
   int granularity = atoi(argv[8]);
 
-  pthread_t *integral_threads = new pthreads_t[nbthreads];
+  pthread_mutex_init(&mutex_chunk,NULL);
+  pthread_mutex_init(&mutex_iter,NULL);
+  pthread_mutex_init(&task_getter_mutex,NULL);
+
+  pthread_t *integral_threads = new pthread_t[nbthreads];
+
 
   float pre_product = (b-a)/n;
 
 
+  next_available_beginning = 0;
+  float mysum=0;
+  if(granularity<n)
+	  next_available_end = 0 + granularity;
+  else
+	  next_available_end = n;
+  cout<<sync;
   if(strcmp(sync,"iteration")==0)
   {
+	  cout<<"iteration reached";
+    for(int i=0;i<nbthreads;i++)
+	    pthread_create(&integral_threads[i],NULL,worker_threads_iteration,(void*)&mysum);
+    
+    return 0;
+  
+    for(int i =0;i<nbthreads;i++)
+	  pthread_join(integral_threads[i],NULL);
+
+
+  cout<<summed_value*pre_product;
+    
+    
+    
   }
 
   else if(strcmp(sync,"chunk")==0)
   {
+    for(int i=0;i<nbthreads;i++)
+	    pthread_create(&integral_threads[i],NULL,worker_threads_chunk,(void*)&mysum);
+  for(int i =0;i<nbthreads;i++)
+	  pthread_join(integral_threads[i],NULL);
+  cout<<summed_value*pre_product;
   }
 
   else if(strcmp(sync,"thread")==0)
   {
+    for(int i=0;i<nbthreads;i++)
+	    pthread_create(&integral_threads[i],NULL,worker_threads_thread,(void*)&mysum);
+  for(int i =0;i<nbthreads;i++)
+	  pthread_join(integral_threads[i],NULL);
   }
 
   else
+  {
 	  cout<<endl<<" Give a valid sync value "<<endl;
+	  return -1;
+  }
+
+
+
   
   
 
 
 
   return 0;
+  
 }
